02347203886056bafea22f37ec19bc6168a6ac7d
Ryan Clark <ryanclark2k@gmail.com>
2019-10-02 22:13:56 -0700

    Update to fix mobile bug.

--- js/blackboard.js
+++ js/blackboard.js
@@ -6,7 +6,7 @@ var colorChooser = document.getElementById('colorChooser');
 
 var mouseDown = false;
 var paintColor = "#FFFFFF";
-var brushSize = 5;
+var brushSize = 4;
 var maxBrushSize = 20;
 var minBrushSize = 1;
 
@@ -151,14 +151,17 @@ function redo(ctx){
     undoStack.push(buffer);
 }
 
-// credit Epistemex @ Stack Overflow
-function getMousePos(canvas, evt) {
-    var rect = canvas.getBoundingClientRect();
+// credit user1693593 @ Stack Overflow
+function  getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect(), // abs. size of element
+        scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
+        scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
+  
     return {
-      x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-    };
-}
+      x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
+      y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+    }
+  }
 
 // credit Phrogz @ Stack Overflow
 function copyCanvasRegionToBuffer( canvas, bufferCanvas ){
@@ -169,4 +172,5 @@ function copyCanvasRegionToBuffer( canvas, bufferCanvas ){
     return bufferCanvas;
   }
 
+addListeners(board);
 addListeners(board2);
